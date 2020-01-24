260c5c42012d35be49141f292e6a899bdbbcf808
alfiankhusnul <alfiankhusnul@gmail.com>
2020-01-24 09:15:29 +0700

    menambah auth controller dan config

---
+++ application/controllers/Auth.php
@@ -0,0 +1,54 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller 
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('model_auth');
+        $this->load->library('form_validation');
+    }
+
+    public function login()
+    {
+        /**
+         * TODO: Nanti tambahin Session
+         * to: Alfian
+         */
+
+        $this->form_validation->set_rules('email', 'Email', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+
+        if ($this->form_validation->run() == TRUE) {
+            $email = $this->input->post('email');
+            $password = $this->input->post('password');
+            if ($this->model_auth->login($email,$password)) {
+                # berhasil login
+                echo "berhasil login";
+            } else {
+                # gagal login
+                echo "gagal login";
+            }
+        } else {
+            $this->load->view('auth_view');
+        }     
+    }
+
+    public function logout()
+    {
+        $this->session->sess_destroy();
+		redirect('auth/login', 'refresh');
+    }
+
+    public function regis()
+    {
+        $nama = $this->input->post("nama");
+        $email = $this->input->post("email");
+        $hash_password = $this->model_auth->makeHash($this->input->post("password"));
+
+        $this->load->helper('array');
+        echo $nama."</br>".$email."</br>".$hash_password;
+
+    }
+}
---
+++ application/views/regis_view.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+		integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+	<title>Regis</title>
+</head>
+<body class="container">
+
+<form action="<?php echo base_url("auth/regis"); ?>" method="post">
+
+<div class="form-group">
+    <label for="inputNama">Nama</label>
+    <input type="text" class="form-control" id="nama" name="nama" aria-describedby="namaHelp">
+</div>
+<div class="form-group">
+    <label for="inputEmail">Email</label>
+    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
+</div>
+<div class="form-group">
+    <label for="inputPassword">Password</label>
+    <input type="password" class="form-control" id="password" name="password" aria-describedby="passwordHelp">
+</div>
+<button type="submit" class="btn btn-primary">Masuk</button>
+
+</form>
+    
+</body>
+</html>
