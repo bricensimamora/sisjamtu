5267bb343a0e3fd02aea433e44219f5078a54cc0
alfiankhusnul <16.8990@stis.ac.id>
2020-06-02 00:16:55 +0700

    3b5_4 bisa insert

--- application/views/kuesioner/tabel_3b5-4.php
+++ application/views/kuesioner/tabel_3b5-4.php
@@ -4,39 +4,68 @@
 <!-- Nav Breadcrumb -->
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item active" aria-current="page">Luaran Penelitian/PkM Lainnya oleh DTPS</li>
+            <li class="breadcrumb-item active" aria-current="page">Luaran Penelitian/PkM Lainnya oleh DTPS (Buku Ber-ISBN, Book Chapter)</li>
         </ol> 
     </nav>
 <!-- End Nav Breadcrumb -->
 
-<form action="" method="post">
-    <hr>
-    <h5>Buku Ber-ISBN, Book Chapter</h5>
-    <div class="form-group row">
-        <label for="nama" class="col-sm-4 col-form-label">1. Luaran Penelitian dan PkM</label>
-        <div class="col-sm-8">
-            <input type="text" class="form-control" id="nama">
-        </div>
-    </div>
-    <div class="form-group row">
-        <label for="nama" class="col-sm-4 col-form-label">2. Tahun</label>
-        <div class="col-sm-8">
-            <input type="text" class="form-control" id="nama">
-        </div>
-    </div>
-    <div class="form-group row">
-        <label for="nama" class="col-sm-4 col-form-label">3. Keterangan</label>
-        <div class="col-sm-8">
-            <input type="text" class="form-control" id="nama">
-        </div>
+<form action="<?= base_url('kuesioner/tabel_3b5_4');?>" method="post">
+    
+    <div class="table-responsive">
+        <table class="table">
+        <thead style="text-align: center;">
+            <tr>
+                <th style="text-align:center;">Penelitian dan PkM</th>
+                <th>Tahun</th>
+                <th>Keterangan</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody id="isian">
+            <tr>
+                <td><input type="text" class="form-control" name="nama[]" id="nama" placeholder="Penelitian"></td>
+                <td><input type="number" min="1990" class="form-control" name="tahun[]" id="tahun"></td>
+                <td><input type="text" class="form-control" name="keterangan[]" id="keterangan"></td>
+                <td><a hidden="true" href="#">X</a></td>
+            </tr>
+        </tbody>
+        </table>
     </div>
 
     <div class="form-group row">
         <div class="col-sm-10">
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <input type="submit" class="btn btn-primary" value="Submit" name="submit">
+            <button type="button" id="add" class="btn btn-success">Tambah Baris</button>
         </div>
     </div>
 </form>
+<script>
+    $(document).ready(function(e){
+        //Variable
+        var html = '<tr>'+
+                '<td><input type="text" class="form-control" name="nama[]" id="nama" placeholder="Penelitian"></td>'+
+                '<td><input type="number" min="1990" class="form-control" name="tahun[]" id="tahun"></td>'+
+                '<td><input type="text" class="form-control" name="keterangan[]" id="keterangan"></td>'+
+                '<td><a href="#" id="remove">X</a></td>'+
+            '</tr>';
+        var maxRows = 10;
+        var x = 1;
+
+        //Add baris ke formulir
+        $("#add").click(function(e){
+            if(x <= maxRows){
+                $("#isian").append(html);
+                x++;
+            }
+        });
+
+        //Hapus baris dari formulir
+        $("#isian").on('click','#remove',function(e){
+            $(this).closest('tr').remove();
+            x--;
+        })
+    });
+</script>
 
 <!-- End Content Section -->
 </div>
